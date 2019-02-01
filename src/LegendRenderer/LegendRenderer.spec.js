bedc1b0836d4790abb8a2313ed0666f455ae308b
Andreas Schmitz <schmitz@terrestris.de>
2019-02-01 16:52:03 +0100

    Adds tiny initial test

---
+++ src/LegendRenderer/LegendRenderer.spec.js
@@ -0,0 +1,11 @@
+/*eslint-env jest*/
+
+import LegendRenderer from './LegendRenderer.js';
+
+describe('LegendRenderer', () => {
+
+  it('is defined', () => {
+    expect(LegendRenderer).not.toBeUndefined();
+  });
+
+});
