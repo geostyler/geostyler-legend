d5dab2311d59920eb8cfa5b141262cae8bd8d0ed
Andreas Schmitz <schmitz@terrestris.de>
2019-02-05 11:55:19 +0100

    Adds a few tests

--- src/LegendRenderer/LegendRenderer.spec.js
+++ src/LegendRenderer/LegendRenderer.spec.js
@@ -8,4 +8,41 @@ describe('LegendRenderer', () => {
     expect(LegendRenderer).not.toBeUndefined();
   });
 
+  it('can be constructed', () => {
+    const renderer = new LegendRenderer();
+    expect(renderer).not.toBeUndefined();
+  });
+
+  it('can convert config objects', () => {
+    const renderer = new LegendRenderer();
+    const config = renderer.extractConfigFromStyle({
+      rules: [{
+        name: 'Legend item 1'
+      }],
+      name: 'Legend 1'
+    });
+    expect(config.title).toBe('Legend 1');
+    expect(config.items[0].title).toBe('Legend item 1');
+  });
+
+  it('can convert config objects without style name', () => {
+    const renderer = new LegendRenderer();
+    const config = renderer.extractConfigFromStyle({
+      rules: [{
+        name: 'Legend item 1'
+      }]
+    });
+    expect(config.title).toBe(undefined);
+  });
+
+  it('will not throw when constructing a rule icon', () => {
+    const renderer = new LegendRenderer();
+    expect(() => renderer.getRuleIcon({
+      symbolizers: [{
+        kind: 'Mark',
+        wellKnownName: 'Circle'
+      }]
+    })).not.toThrow();
+  });
+
 });
