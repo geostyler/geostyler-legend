3aa0f8db1d39afe2f76d318b46d313da731e3d6f
Andreas Schmitz <schmitz@terrestris.de>
2019-02-11 14:21:21 +0100

    Rename misleading index.d.ts filename, improve config

--- oltypes.d.ts
+++ oltypes.d.ts
@@ -1,8 +1,6 @@
 declare module 'ol/extent';
 declare module 'ol/geom/Geometry';
-declare module 'ol/style/Style' {
-    export = ol.style.Style;
-}
+declare module 'ol/style/Style';
 declare module 'ol/geom/Polygon';
 declare module 'ol/geom/LineString';
 declare module 'ol/render/canvas/Immediate';
