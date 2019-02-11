ef14f8fa8d751bec26fd561751ac8800c0044bb2
Andreas Schmitz <schmitz@terrestris.de>
2019-02-11 12:59:46 +0100

    Use typescript

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+declare module 'ol/extent';
+declare module 'ol/geom/Geometry';
+declare module 'ol/style/Style' {
+    export = ol.style.Style;
+}
+declare module 'ol/geom/Polygon';
+declare module 'ol/geom/LineString';
+declare module 'ol/render/canvas/Immediate';
+declare module 'ol/transform';
