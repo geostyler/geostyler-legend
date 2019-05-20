87c79c196700cd54ad6e69409e6c5dab76ab5854
Andreas Schmitz <schmitz@terrestris.de>
2019-05-20 16:36:28 +0200

    Update dependencies

--- oltypes.d.ts
+++ oltypes.d.ts
@@ -2,6 +2,7 @@ declare module 'ol/extent';
 declare module 'ol/geom/Geometry';
 declare module 'ol/style/Style';
 declare module 'ol/geom/Polygon';
+declare module 'ol/geom/Point';
 declare module 'ol/geom/LineString';
 declare module 'ol/render/canvas/Immediate';
 declare module 'ol/transform';
